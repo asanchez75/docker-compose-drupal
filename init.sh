476dfd40a33f73fd9cfae02450ddf05e75d77b34
Adam <a.sanchez75@gmail.com>
2019-10-14 16:44:51 +0200

    added chmodd

--- init.sh
+++ init.sh
@@ -2,7 +2,9 @@
 
 if [ ! -f ./.loaded ]; then
   cp -Rf /drupal-8.x/. /var/www/html/
-  touch ./.loaded
+  chmod 777 /var/www/html/sites/default/files  
+  chmod 777 /var/www/html/sites/default/settings.php 
+ touch ./.loaded
 fi
 
 bash -c "apachectl -D FOREGROUND"
